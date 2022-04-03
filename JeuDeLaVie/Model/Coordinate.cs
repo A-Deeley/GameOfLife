11f5dfdd21d0409f20a8986a0786180d24a9169d
Andrew Deeley <adeeley98@gmail.com>
2022-04-03 12:27:37 -0400

    Ajout des models de base.

---
+++ JeuDeLaVie/Model/Coordinate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuDeLaVie.Model
+{
+    internal class Coordinate
+    {
+
+        private int X { get; set; }
+        private int Y { get; set; }
+        public int PosX { get => X * TileWidth; }
+        public int PosY { get => Y * TileWidth; }
+        public int TileWidth { get; private set; }
+
+        /// <summary>
+        /// Creates a Coordinate object that represents a location on a canvas control. Default tile width of 15px.
+        /// </summary>
+        /// <param name="x">x position</param>
+        /// <param name="y">y position</param>
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+            TileWidth = 15;
+        }
+
+        /// <summary>
+        /// Set the tile width modifier.
+        /// </summary>
+        /// <param name="width">New value for the tile width. Must be greater than 15 (pixels).</param>
+        public void SetTileWidth(int width)
+        {
+            if (width > 15)
+                TileWidth = width;
+        }
+    }
+}
---
+++ JeuDeLaVie/Model/LifeForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuDeLaVie.Model
+{
+    internal class LifeForm
+    {
+        public bool IsAlive { get; set; }
+        public Coordinate Coord { get; private set; }
+
+        /// <summary>
+        /// Create a LifeForm object with default tile width.
+        /// </summary>
+        /// <param name="x">x position of the form</param>
+        /// <param name="y">y position of the form</param>
+        public LifeForm(int x, int y)
+        {
+            IsAlive = false;
+            Coord = new Coordinate(x, y);
+        }
+
+        /// <summary>
+        /// Create a LifeForm object with a specific tile width.
+        /// </summary>
+        /// <param name="x">x position of the form.</param>
+        /// <param name="y">y position of the form.</param>
+        /// <param name="tileWidth">Tile width to set. Must be greater than 15.</param>
+        public LifeForm(int x, int y, int tileWidth)
+            :this(x, y)
+        {
+            Coord.SetTileWidth(tileWidth);
+        }
+    }
+}
